a7372f5f90a556043d783d352fb477447eca7704
adslppp <1759215751@qq.com>
2021-06-11 19:08:54 +0800

    修改属性类型，isxx是布尔型

--- src/model/Device.py
+++ src/model/Device.py
@@ -1,16 +1,17 @@
+from pydantic.errors import BoolError
 from src.model.BaseModel import BaseModel, database
 from peewee import *
 
 
 class Device(BaseModel):
     roomID = CharField(primary_key=True)
-    isPower = IntegerField()
+    isPower = BooleanField()
     targetTemperature = IntegerField()
     currentTemperature = IntegerField()
     # 0表示低风、1表示中风、2表示高风
     windSpeed = IntegerField()
-    isAskAir = IntegerField()
-    isSupplyAir = IntegerField()
+    isAskAir = BooleanField()
+    isSupplyAir = BooleanField()
     cost = FloatField()
 
     class Meta:
