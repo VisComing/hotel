5647f44b56367eabcdac9eed796dbf9786e8f155
adslppp <1759215751@qq.com>
2021-06-11 17:31:05 +0800

    修改数据库

--- src/model/DetailList.py
+++ src/model/DetailList.py
@@ -3,11 +3,11 @@ from peewee import *
 from src.model.Order import Order
 
 
-class DetailList(BaseModel):
+class DetailListItem(BaseModel):
     orderID = ForeignKeyField(
-        Order, field="orderID", backref="DetailLists", db_column="orderID"
+        Order, field="orderID", backref="DetailListItems", db_column="orderID"
     )
-    DetailListID = TextField()
+    detailListID = TextField()
     startTime = DateTimeField()
     endtime = DateTimeField()
     # 0表示低风、1表示中风、2表示高风
@@ -16,5 +16,5 @@ class DetailList(BaseModel):
     billingRate = FloatField()
 
     class Meta:
-        primary_key = CompositeKey("DetailList", "startTime")
-        table_name = "tbDetailList"
+        primary_key = CompositeKey("detailListID", "orderID")
+        table_name = "tbDetailListItem"
