1e9d6f7890ba3eedc1fed54397dbcb7b7fed98de
adslppp <1759215751@qq.com>
2021-06-12 23:41:34 +0800

    roomUpdate完成

--- src/admin/RoomStateUpdateHandler.py
+++ src/admin/RoomStateUpdateHandler.py
@@ -1,4 +1,8 @@
 import asyncio
+import logging
+from src.model import *
+from jsonrpcclient.clients.websockets_client import WebSocketsClient
+import websockets
 
 
 class RoomStateUpdateHandler:
@@ -8,11 +12,33 @@ class RoomStateUpdateHandler:
     async def roomStateUpdate(self, websocket) -> None:
         # TODO @adslppp
         """
-        roomStateUpdate [summary]
+        roomStateUpdate 每隔一秒更新房间信息
 
         Args:
-            websocket ([type]): [description]
+            websocket ([type]): 传入此连接
         """
         while True:
-            pass
+            # 选出所有的房间
+            devices = await DBManager.execute(Device.select())
+            infos = list()
+            for device in devices:
+                info = dict()
+                info["roomID"] = device.roomID
+                info["isStarted"] = device.isPower
+                # 如果空调没开，就不传其余字段
+                if device.isPower == True:
+                    info["roomTemperature"] = device.currentTemperature
+                    info["targetTemperature"] = device.targetTemperature
+                    info["windSpeed"] = device.windSpeed
+                    info["isSupplyingWind"] = device.isSupplyAir
+                infos.append(info)
+            try:
+                await WebSocketsClient(websocket).notify(
+                    method_name="roomStateUpdate", infos=infos
+                )
+            except websockets.exceptions.ConnectionClosedError as e:
+                # 客户端断开连接异常
+                logging.warning(e)
+                # 退出while循环
+                break
             await asyncio.sleep(1)
